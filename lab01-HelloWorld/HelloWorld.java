28b0a69f0e9db503fae75e2e77839be29f364fd9
yus217 <yus217@lehigh.edu>
2016-01-29 17:17:40 +0000

    updated Hello World

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//Yuecheng Shi
+//Demo 0
+//Hello World Tave Program
+//
+//first compile the program
+//      javac HelloWorld Java.java
+//run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+
+//add main method
+ public static void main(String[] Args) {
+     
+//print the statement with box around it
+ System.out.println("########################");
+ System.out.println("########################");
+ System.out.println("#                      #");
+ System.out.println("#  Hello World 2016!!  #");            
+ System.out.println("#                      #");
+ System.out.println("########################");
+ System.out.println("########################");
+ System.out.println("test");
+ 
+ // test/testtest
+  }
+ }
