472abd30a16406ead09926569679b5177ae73a09
yus217 <yus217@lehigh.edu>
2016-04-22 02:09:43 +0000

    update comment

--- hw12/CSE2Linear.java
+++ hw12/CSE2Linear.java
@@ -1,121 +1,92 @@
-package hw12;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
 
-
-
-public class CSE2Linear {
-	
-		  
-		 public static int Counter = 1;   
-		 public static void main(String args[])  
-		 {  
-			 int Counter2=0;
-			 System.out.print("Enter 15 ints for final grades in CSE2: ");
-			 int[] Data =new int[15];
-			 Scanner sc=new Scanner(System.in);
-			 
-			 for(int i=0;i<15;i++)
-			 {
-				 Data[i]=sc.nextInt(); 
-			 }
-			 System.out.print("Enter a grade to search for:");
-			 int KeyValue = sc.nextInt();  
-			
-			 if (Linear_Search((int) KeyValue,Data))  
-			 {  
-				   
-				 System.out.println("Sorted");
-				 Arrays.sort(Data);
-				 for(int i=0;i<15;i++)
-				 {
-					 System.out.print(Data[i]+" ");
-				 }
-				 System.out.println();  
-				 System.out.println(KeyValue+" was Found in the list with "+Counter+" iterations");
-				 Counter2=Counter;
-			 }  
-			 else  
-			 {  
-				   
-				 System.out.println("Sorted");
-				 Arrays.sort(Data);
-				 for(int i=0;i<15;i++)
-				 {
-					 System.out.print(Data[i]+" ");
-				 }
-				 System.out.println();
-				 System.out.println(KeyValue+" was not Found in the list with "+Counter+" iterations");
-				 Counter2=Counter;
-			 }  
-			 System.out.println("Scrambled:");
-			 List list=new ArrayList<>();
-			 for(int i=0;i<15;i++)
-			 {
-				 list.add(Data[i]);
-			 }
-//			 List list = Arrays.asList(Data);
-     		 Collections.shuffle(list);
-			 Iterator ite = list.iterator();    
-			 while(ite.hasNext())
-			 {    
-				 System.out.print(ite.next().toString()+" ");    
-		     }
-			 int[] Data2=new int[15];
-			 for(int i=0;i<15;i++)
-			 {
-				 Data2[i]=(int) list.get(i);
-			 }
-			 System.out.println();
-			 System.out.print("Enter a grade to search for:");
-			 int KeyValue2 = sc.nextInt();  
-			  
-			 if (Linear_Search((int) KeyValue2,Data2))  
-			 {  
-				   
-				 System.out.println("Sorted");
-				 Arrays.sort(Data2);
-				 for(int i=0;i<15;i++)
-				 {
-					 System.out.print(Data2[i]+" ");
-				 }
-				 System.out.println();  
-				 System.out.println(KeyValue2+" was Found in the list with "+(Counter-Counter2+1)+" iterations");  
-			 }  
-			 else  
-			 {  
-				 
-				 System.out.println("Sorted");
-				 Arrays.sort(Data2);
-				 for(int i=0;i<15;i++)
-				 {
-					 System.out.print(Data2[i]+" ");
-				 }
-				 System.out.println();
-				 System.out.println(KeyValue+" was not Found in the list with "+(Counter-Counter2+1)+" iterations");  
-			 }  
-		 }  
-		  
-		 // ---------------------------------------------------  
-		 
-		 // ---------------------------------------------------  
-		 public static boolean Linear_Search(int Key,int[] Data)  
-		 {  
-			 int i;  
-			 for (i = 0; i < 15; i++)  
-			 {  
-				 
-//				 System.out.print( (int) Data[i]+" " );  
-				  
-				 if ((int) Key == (int) Data[i])  
-					 return true;   
-				 Counter++;   
-			 }  
-			 return false;  
-		 }  
+public class CSE2Linear{
+  int num;
+  int[] binary;
+  Random random;
+  int count;
+  Scanner sc = new Scanner(System.in);
+   
+  public CSE2Linear(int[] n){
+    binary = n;
+    num = n.length;
+  }
+  
+  public CSE2Linear enterData(){
+    for(int i=0; i<num; i++){
+      if(sc.hasNextInt()){
+        binary[i] = sc.nextInt();
+        if(binary[i] <1 || binary[i]>100){
+          System.out.println("not in the range");
+          break;
+        }
+        }
+      else{
+        System.out.println("not an integer");
+        break;
+      }  
+    }
+    return new CSE2Linear(binary);
+  }
+  
+  
+  public String search(){
+    int grade = sc.nextInt();
+    int  init = 0;
+    int len = binary.length -1;
+    count = 0;
+    String result ="";
+    
+    while(len >= init){
+      int middle = init + (len - init) /2;
+      if(grade < binary[middle]){
+        len = middle -1;
+      }else if(grade > binary[middle]){
+        init = middle + 1;
+      }else{
+        return result += binary[middle];
+      }
+      count++;
+    }
+    return result += grade + " was not found in the list with "+ count +" iterations";
+  }
+  
+  public CSE2Linear Scrambled(){
+    random = new Random();
+    int rand;
+    for(int i=0;i<num;i++){
+      rand = random.nextInt(num);
+      int temp = binary[i];
+      binary[i] = binary[rand];
+      binary[rand] = temp;
+    }
+    return new CSE2Linear(binary);
+  }
+  
+  public void tostring(){
+    String result = "[";
+    for(int i=0; i<num-1; i++){
+      result += binary[i] +", ";
+    }
+    result += binary[num-1]+"]";
+    System.out.println(result);
+  }
+  
+  public CSE2Linear sort(){
+    Arrays.sort(binary);
+    return new CSE2Linear(binary);
+    
+  }
+  
+  public static void main(String[] args){
+    CSE2Linear c = new CSE2Linear(new int[15]);
+    c.enterData().tostring();
+    System.out.println(c.search());
+    c.Scrambled().tostring();
+    c.sort().tostring();
+    System.out.println(c.search());
+  }
 }
